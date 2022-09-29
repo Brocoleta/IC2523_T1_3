6a80c2129454a76455924d738eaed272b5552bbe
Brocoleta <pbrancoli@uc.cl>
2022-09-29 12:15:51 -0300

    listo

--- philosophers.go
+++ philosophers.go
@@ -6,29 +6,35 @@ import (
 	"time"
 )
 
+
+// Los tenedores eran un Lock que simula si esta siendo usado o no
 type fork struct{ sync.Mutex }
 
+
+// El host va a ser un Lock que simula si hay alguien comiendo o no
 var host struct{ sync.Mutex }
 
+// Un filosofo tendra 4 atributos, el id, los dos tenedores, el derecho y el izquierdo, y la variable eaten
+// que representa cuantas veces ha comido
 type philosopher struct {
 	id                  int
 	leftFork, rightFork *fork
 	eaten				int
 }
 
-// Goes from thinking to hungry to eating and done eating then starts over.
-// Adapt the pause values to increased or decrease contentions
-// around the forks.
+
 func (p philosopher) eat() {
 	
 	for j := 0; j < 2; j++ {
+		// Primero el host bloquea para que nadie mas pueda estar comiendo a la vez
 		host.Lock()
+		// Se bloquean los dos cubiertos del filosofo
 		p.leftFork.Lock()
 		p.rightFork.Lock()
 		time.Sleep(time.Second)
 		fmt.Println("Filosofo",p.id+1,"comiendo")
 		time.Sleep(time.Second)
-
+		// Cuando el filosofo termina de comer, se desocupan los tenedores respectivos, y el host acepta que otra persona pueda comer
 		p.rightFork.Unlock()
 		p.leftFork.Unlock()
 		host.Unlock()
@@ -47,7 +53,7 @@ func (p philosopher) eat() {
 var eatWgroup sync.WaitGroup
 
 func main() {
-	// How many philosophers and forks
+	// Inicializamos los tenedores y los filosofos
 
 	count := 5
 
@@ -58,7 +64,7 @@ func main() {
 		forks[i] = new(fork)
 	}
 
-	// Create philospoher, assign them 2 forks and send them to the dining table
+	// Para cada filosofo le asignamos los tenedores segun corresponda
 	philosophers := make([]*philosopher, count)
 	for i := 0; i < count; i++ {
 		philosophers[i] = &philosopher{
